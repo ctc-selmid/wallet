9ec7ee5a8d077049bb9191466008b231c16e0df4
ookimaki <noreply@ookimaki.com>
2021-10-12 21:35:02 +0900

    fix: storybook import issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,26 @@ const path = require("path");
 module.exports = {
   stories: ["../src/components/**/**/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+
   webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": path.join(__dirname, "../node_modules/@emotion/react"),
-          "emotion-theming": path.join(__dirname, "../node_modules/@emotion/react"),
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            plugins: [require.resolve("@babel/plugin-proposal-optional-chaining")],
+          },
         },
-      },
+      ],
+      include: require.resolve("@decentralized-identity/ion-tools"),
+    });
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@emotion/core": require.resolve("@emotion/react"),
+      "emotion-theming": require.resolve("@emotion/react"),
+      jose: path.join(__dirname, "../node_modules/jose/dist/node/cjs"),
     };
+    return config;
   },
 };
